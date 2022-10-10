920c6eeb55a528237390ab139be5dac255159bf7
İsmail Can Yıldırım <ismailcan5106@gmail.com>
2022-10-11 00:03:08 +0300

    Yarışma için son kodlar yazıldı

--- AnaAviyonikRoket.ino
+++ AnaAviyonikRoket.ino
@@ -1,153 +1,199 @@
-#include <MPU9250_asukiaaa.h>
-#include <BME280I2C.h>
+/*#if !defined(ARDUINO_ARCH_RP2040)
+  #error For RP2040 only
+#endif
+#if defined(ARDUINO_ARCH_MBED)  
+  #define PIN_SD_MOSI       PIN_SPI_MOSI
+  #define PIN_SD_MISO       PIN_SPI_MISO
+  #define PIN_SD_SCK        PIN_SPI_SCK
+  #define PIN_SD_SS         PIN_SPI_SS
+#else
+  #define PIN_SD_MOSI       PIN_SPI0_MOSI
+  #define PIN_SD_MISO       PIN_SPI0_MISO
+  #define PIN_SD_SCK        PIN_SPI0_SCK
+  #define PIN_SD_SS         PIN_SPI0_SS 
+#endif
+#define _RP2040_SD_LOGLEVEL_       */
+int t1 = 14;
+int t2 = 3;
 #include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+#include <MPU9250_asukiaaa.h>
 #include <TinyGPS.h>
 #include <SoftwareSerial.h>
+#include <MovingAverageFilter.h>
+#include <SPI.h>
+//#include <RP2040_SD.h>
 
+#define SEALEVELPRESSURE_HPA (877.20)
+float aX, aY, aZ, gX, gY, gZ, mX, mY, mZ;
+static void smartdelay(unsigned long ms);
 
+Adafruit_BME280 bme;
 MPU9250_asukiaaa mySensor;
-BME280I2C bme;
 TinyGPS gps;
-SoftwareSerial ss(13,12);
+SoftwareSerial ss(11,12);
 SoftwareSerial lora(1,0);
+//File myFile;
 
-float aX, aY, aZ, aSqrt, gX, gY, gZ, mDirection, mX, mY, mZ;
-String gpsKonumStr, gpsSatelliteStr , gpsAltitudeStr ;
-String Data0,Data1,Data2,Data3,Data4,Data5,Data6 = "";
+MovingAverageFilter mvg00(20);
+MovingAverageFilter mvg01(20);
+MovingAverageFilter mvg02(20);
+MovingAverageFilter mvg03(20);
 
-unsigned long chars;
-unsigned short sentences, failed_checksum;
-float bmp[3]={0.0,0.0,0.0};
+MovingAverageFilter mvg10(20);
+MovingAverageFilter mvg11(20);
+MovingAverageFilter mvg12(20);
+
+MovingAverageFilter mvg20(20);
+MovingAverageFilter mvg21(20);
+MovingAverageFilter mvg22(20);
+
+MovingAverageFilter mvg30(20);
+MovingAverageFilter mvg31(20);
+MovingAverageFilter mvg32(20);
 
 void setup() {
-  Serial.begin(115200);
-  ss.begin(115200);
-  lora.begin(9600);
+  pinMode(25 , OUTPUT);
+  pinMode(t1,OUTPUT);
+  pinMode(t2, OUTPUT);
+  buzzer(10, 50);
+  
+  Serial.begin(9600);
+  bme.begin(0x76);
   mySensor.beginAccel();
   mySensor.beginGyro();
   mySensor.beginMag();
-  Wire.begin();
-  bme.begin();
+  ss.begin(9600);
+  lora.begin(9600);
+ // SD.begin(PIN_SD_SS);
+  #define fileName  "anah.txt"
+  
+  
   // You can set your own offset for mag values
   // mySensor.magXOffset = -50;
   // mySensor.magYOffset = -55;
   // mySensor.magZOffset = -10;
 }
 
-void loop() {
-  
-   mpu();
-   printBME280Data(&Serial);
-   dataGonder();
-   delay(500); 
-  
-  
-} 
-void printBME280Data
-(
-   Stream* client
-)
+void loop() 
 {
-   float temp(NAN), hum(NAN), pres(NAN);
-
-   BME280::TempUnit tempUnit(BME280::TempUnit_Celsius);
-   BME280::PresUnit presUnit(BME280::PresUnit_Pa);
-
-   bme.read(pres, temp, hum, tempUnit, presUnit);
-
-   client->print("Temp: ");
-   client->print(temp);
-   client->print("°"+ String(tempUnit == BME280::TempUnit_Celsius ? 'C' :'F'));
-   client->print("\t\tHumidity: ");
-   client->print(hum);
-   client->print("% RH");
-   client->print("\t\tPressure: ");
-   client->print(pres);
-   client->println("Pa");
-
-
-   delay(1000);
-} 
-
-void mpu() {  
-  uint8_t sensorId;
-  int result;
-
-  result = mySensor.accelUpdate();
-  if (result == 0) {
-    aX = mySensor.accelX();
-    aY = mySensor.accelY();
-    aZ = mySensor.accelZ();
-    aSqrt = mySensor.accelSqrt();
-    Serial.println("accelX: " + String(aX));
-    Serial.println("accelY: " + String(aY));
-    Serial.println("accelZ: " + String(aZ));
-    Serial.println("accelSqrt: " + String(aSqrt));
-  } else {
-    Serial.println("Cannod read accel values " + String(result));
-  }
-
-  result = mySensor.gyroUpdate();
-  if (result == 0) {
-    gX = mySensor.gyroX();
-    gY = mySensor.gyroY();
-    gZ = mySensor.gyroZ();
-    Serial.println("gyroX: " + String(gX));
-    Serial.println("gyroY: " + String(gY));
-    Serial.println("gyroZ: " + String(gZ));
-  } else {
-    Serial.println("Cannot read gyro values " + String(result));
-  }
-
-  result = mySensor.magUpdate();
-  if (result == 0) {
-    mX = mySensor.magX();
-    mY = mySensor.magY();
-    mZ = mySensor.magZ();
-    mDirection = mySensor.magHorizDirection();
-    Serial.println("magX: " + String(mX));
-    Serial.println("maxY: " + String(mY));
-    Serial.println("magZ: " + String(mZ));
-    Serial.println("yatay yön: " + String(mDirection));
-  } else {
-    Serial.println("Cannot read mag values " + String(result));
-  }
+  
+  mpu9250();
+  bmp280();
+  gpsKonum();
+  gpsSatellite();
+  smartdelay(500);
 }
 
-String gpsKonum() {
+void bmp280()
+{
+    float T  = bme.readTemperature();
+    float P = bme.readPressure() / 100.0F;
+    float A = bme.readAltitude(SEALEVELPRESSURE_HPA);
+    float H = bme.readHumidity();
+  
+    float avgT = mvg00.process(T);
+    float avgP = mvg01.process(P);
+    float avgA = mvg02.process(A);
+    float avgH = mvg03.process(H);
+    
+    Serial.println(avgT);
+    Serial.println(avgP);
+    Serial.println(avgA);
+    Serial.println(avgH);
+   /* myFile = SD.open(fileName, FILE_WRITE);
+    myFile.println(avgT);
+    myFile.println(avgP);
+    myFile.println(avgA);
+    myFile.println(avgH);
+    myFile.close();*/
+
+}
+
+void mpu9250()
+{
+    float result;
+  result = mySensor.accelUpdate();
+    float aX = mySensor.accelX();
+    float aY = mySensor.accelY();
+    float aZ = mySensor.accelZ();
+
+    float avgaX = mvg10.process(aX);
+    float avgaY = mvg11.process(aY);
+    float avgaZ = mvg12.process(aZ);
+
+  result = mySensor.gyroUpdate();
+    float gX = mySensor.gyroX();
+    float gY = mySensor.gyroY();
+    float gZ = mySensor.gyroZ();
+
+    float avggX = mvg20.process(gX);
+    float avggY = mvg21.process(gY);
+    float avggZ = mvg22.process(gZ);
+
+  result = mySensor.magUpdate();
+    float mX = mySensor.magX();
+    float mY = mySensor.magY();
+    float mZ = mySensor.magZ();
+
+    float avgmX = mvg30.process(mX);
+    float avgmY = mvg31.process(mY);
+    float avgmZ = mvg32.process(mZ);
+
+    Serial.println(aX);
+    Serial.println(aY);
+    Serial.println(aZ);
+    Serial.println(gX);
+    Serial.println(gY);
+    Serial.println(gZ);
+    Serial.println(mX);
+    Serial.println(mY);
+    Serial.println(mZ);
+  /*  myFile = SD.open(fileName, FILE_WRITE);
+    myFile.println(avgaX);
+    myFile.println(avgaY);
+    myFile.println(avgaZ);
+    myFile.println(avggX);
+    myFile.println(avggY);
+    myFile.println(avggZ);
+    myFile.println(avgmX);
+    myFile.println(avgmY);
+    myFile.println(avgmZ);
+    myFile.close();*/
+}
+
+void gpsKonum() {
   float flat, flon, invalid;
   gps.f_get_position(&flat, &flon);
   invalid = TinyGPS::GPS_INVALID_F_ANGLE;
+  
   if (flat == invalid || flon == invalid) {
-    return ("NULL");
+    lora.println(float(0),6);
+    lora.println(float(0),6);
   } else {
-    String latitude = String(flat, 6);
-    String longitude = String(flon, 6);
-    return (latitude + " " + longitude);
+    lora.println(float(flat),6);
+    lora.println(float(flon),6);
+  /*  myFile = SD.open(fileName, FILE_WRITE);
+    myFile.println(float(flat),6);
+    myFile.println(float(flon),6);
+    myFile.close();*/
   }
 }
-String gpsSatellite() {
+void gpsSatellite() {
   float satellite, invalid;
   satellite = gps.satellites();
   invalid   = TinyGPS::GPS_INVALID_SATELLITES;
   if (satellite == invalid) {
-    return ("NULL");
+    lora.println(float(0.00));
   } else {
-    return (String(satellite)); 
+    lora.println(satellite);
+/*    myFile = SD.open(fileName, FILE_WRITE);
+    myFile.println(satellite);
+    myFile.close();*/
+    
   }
 }
-
-String gpsAltitude(){
-  float gpsAltitude , invalid;
-  gpsAltitude = gps.altitude() ;
-  invalid   = TinyGPS::GPS_INVALID_ALTITUDE;
-  if (gpsAltitude == invalid) {
-    return ("NULL");
-  } else {
-    return (String(gpsAltitude)); 
-  }
-}
-
 static void smartdelay(unsigned long ms) {
   unsigned long start = millis();
   do {
@@ -157,37 +203,11 @@ static void smartdelay(unsigned long ms) {
   } while (millis() - start < ms);
 }
 
-void dataGonder() {
-
-
-  gpsKonumStr     = gpsKonum();
-  gpsSatelliteStr = gpsSatellite();
-  gpsAltitudeStr = gpsAltitude();
-  
-  Data0 = "|01-Ana";
-  Data1 = "|GPS" ;
-  Data1 += "|" + gpsSatelliteStr ;
-  Data1 += "|" + gpsKonumStr + "|" ;
-  Data1 += "|" + gpsAltitudeStr + "|" ;
-  Data2 = "| Basinç : 876.00 |" ;
-  Data3 = "| Yükseklik : 0.4 m |"  ;
-  Data4 = "| Gyro X : " + String(gX) +"| Gyro Y : " + String(gY) +"|  Gyro Z : " + String(gZ) ;
-  Data5 = "| İvmeX : " + String(mX) +"| İvmeY : " + String(mY) +"| İvmeZ : " + String(mZ);
-  Data6 = "| aciX : " + String(aX) +"| aciY : "+ String(aY) +"| aciZ : " + String(aZ); 
-
-  Serial.println(Data0);
-  Serial.println(Data1);
-  Serial.println(Data2);
-  Serial.println(Data3);
-  Serial.println(Data4);
-  Serial.println(Data5);
-  Serial.println(Data6);
-
-  lora.println(Data0);
-  lora.println(Data1);
-  lora.println(Data2);
-  lora.println(Data3);
-  lora.println(Data4);
-  lora.println(Data5);
-  lora.println(Data6);
+void buzzer(int tekrar, int sure) {
+  for (int i = 0; i < tekrar; i++) {
+    digitalWrite(25, HIGH);
+    delay(sure);
+    digitalWrite(25, LOW);
+    delay(sure);
+  }
 }
